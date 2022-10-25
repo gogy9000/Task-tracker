561c739d8cf2365bd95836cecba81a8dafd7a545
gogy9000 <goropashnyj9000@gmail.com>
2022-10-25 12:27:56 +0500

    refactoring layout

--- apps/expo/App.tsx
+++ apps/expo/App.tsx
@@ -2,6 +2,7 @@ import { NativeNavigation } from 'app/navigation/native'
 import { CommonProvider } from 'app/provider'
 
 export default function App() {
+  debugger
   return (
     <CommonProvider>
       <NativeNavigation />
