a8b30ba1fa6b4292835c754b5093e4f4b8b445db
gogy9000 <goropashnyj9000@gmail.com>
2022-11-09 21:52:06 +0500

    iconButtons bag fixed

--- packages/app/View/StartDateController.tsx
+++ packages/app/View/StartDateController.tsx
@@ -1,7 +1,7 @@
 import { TaskItem } from 'app/DAL/types/types'
 import { Api } from 'app/DAL/Api'
-import { Icon, IconButton, useDisclose,Text } from 'native-base'
-import  MaterialIcons  from '@expo/vector-icons/MaterialIcons'
+import { Icon, IconButton, useDisclose, Text, ArrowBackIcon, Pressable } from 'native-base'
+import MaterialIcons from '@expo/vector-icons/MaterialIcons'
 import { CommonModal } from 'app/View/commonModal'
 import { TimePicker } from 'app/View/timePicker'
 import React from 'react'
@@ -17,11 +17,12 @@ export const StartDateController = ({ task }: { task: TaskItem }) => {
     onToggle
   } = useDisclose()
 
+
   return (
     <>
-      <IconButton onPress={onToggle} mb='4' variant='solid' bg='indigo.500' colorScheme='indigo' borderRadius='full'
-                  icon={<Icon as={MaterialIcons} name='date-range' size='6' />}
+      <IconButton onPressOut={onToggle}  mb='4' variant='solid' bg='indigo.500' colorScheme='indigo' borderRadius='full'
                   _icon={{
+                    as: MaterialIcons, name: 'date-range',
                     color: 'warmGray.50',
                     _dark: {
                       color: 'warmGray.50'
@@ -29,8 +30,8 @@ export const StartDateController = ({ task }: { task: TaskItem }) => {
                   }}
       />
       <CommonModal showModal={isOpen} setShowModal={onToggle}>
-        <Text>azaza</Text>
-        {/*<TimePicker callback={onPutTask} />*/}
+        {/*<Text>azaza</Text>*/}
+        <TimePicker callback={onPutTask} />
       </CommonModal>
     </>
   )
